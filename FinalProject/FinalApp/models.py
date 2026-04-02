313fe37a5eecc5f427f94a05657a0832a39d261c
Alondrag360 <alondrag4265@gmail.com>
2026-04-02 11:18:12 -0500

    Update models.py
    Resource

--- FinalProject/FinalApp/models.py
+++ FinalProject/FinalApp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class FoodResource(models.Model):
+  RESOURCE_TYPES = [
+    ('DRIVE', 'Food Drive'),
+    ('STORE', 'Economical Store'),
+    ('FREE', 'Free Event'),
+  ]
+
+name = models.CharField(max_length=200)
+address = models.TextField()
+resource_type = models.CharField(max_length=10, choices=RESOURCE_TYPES)
+latitude = models.FloatField()
+longtitude = models.FloatField()
+description = models.TextField(blank=True)
+date_held = models.DateTimeField(null=True, blank=True) 
+
+def_str_(self):
+  return f"{self.name}({self.get_resource_type_display()})"
